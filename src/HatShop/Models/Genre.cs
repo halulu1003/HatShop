91a1355052a0717af7bd04ae36c22753c684cbad
halulu1003 <youyujia1003@outlook.com>
2017-08-17 17:32:51 +1200

    models
    add brand,genre,category

--- src/HatShop/Models/Genre.cs
+++ src/HatShop/Models/Genre.cs
@@ -5,14 +5,28 @@ using System.Threading.Tasks;
 
 namespace HATSHOP.Models
 {
+    public class Brand
+    {
+        public int BrandId { get; set; }
+        public string Name { get; set; }
+    }
     public class Genre
     {
+        public int GenreId { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Brand> Albums { get; set; }
     }
 
     public class Category
     {
+        public int CategoryId { get; set; }
+        public int GenreId { get; set; }
+        public int BrandId { get; set; }
         public string Type { get; set; }
+        public decimal Price { get; set; }
+        public string CategoryArtUrl { get; set; }
         public Genre Genre { get; set; }
+        public Brand Brand { get; set; }
     }
 }
