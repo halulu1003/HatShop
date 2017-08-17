9c01c87094170b6c8abb560182f6523230de257c
halulu1003 <youyujia1003@outlook.com>
2017-08-17 17:41:20 +1200

    seperate brand

---
+++ src/HatShop/Models/Brand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HATSHOP.Models
+{
+    public class Brand
+    {
+        public int BrandId { get; set; }
+        public string Name { get; set; }
+    }
+}
